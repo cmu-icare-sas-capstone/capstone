53db4db36209c50dfab580d3a31a98948675557d
Muyu Tong <61313159+mtong0@users.noreply.github.com>
2022-09-25 18:20:20 -0400

    update databaseio and eda-1

---
+++ web/heatmap.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import L1.ETL.DatabaseIO as dbio
+
+def los_heatmap():
+    sql = \
+        "select \
+            zip_code_3_digits, \
+            round (avg(length_of_stay),0) as avg_los \
+        from cms_medicare_with_covid_risk \
+        where \
+            covid=1 \
+            and primary_diagnosis = 'COVID-19' \
+            and zip_code_3_digits is not null \
+            and zip_code_3_digits != 'OOS' \
+        group by zip_code_3_digits \
+        order by zip_code_3_digits, avg_los;"
+
+    df = dbio.read_from_db(None, sql)
