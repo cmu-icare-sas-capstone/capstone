c627a252df606150c273dccc5d81973de889098f
Muyu Tong <61313159+mtong0@users.noreply.github.com>
2022-10-06 19:41:48 -0400

    test

--- web/components/eda_options_container.py
+++ web/components/eda_options_container.py
@@ -10,23 +10,27 @@ def eda_options_container():
         )
 
     data_counter_container = st.container()
-    # if eda_select_box == "Data Point Count":
-    #     data_counter_container.header("Data Point Count")
-    #
-    #     with data_counter_container:
-    #         group = st.selectbox(
-    #             "Group By",
-    #             options=("Age", "Race", "LOS", "County")
-    #         )
-    #         col1, col2 = st.columns(2)
-    #         if group == "Age":
-    #             show_count_and_describe("age_group", col1, col2)
-    #         elif group == "Race":
-    #             show_count_and_describe("race", col1, col2)
-    #         elif group == "LOS":
-    #             show_count_and_describe("length_of_stay", col1, col2)
-    #         elif group == "County":
-    #             show_count_and_describe("area_name", col1, col2)
+    if eda_select_box == "Data Point Count":
+        data_counter_container.header("Data Point Count")
+
+        with data_counter_container:
+            group = st.selectbox(
+                "Group By",
+                options=("Age", "Race", "LOS", "County")
+            )
+
+            if group == "Age":
+                group_df = basic_eda.group_by("age_group")
+                st.bar_chart(data=group_df)
+            elif group == "Race":
+                group_df = basic_eda.group_by("race")
+                st.bar_chart(data=group_df)
+            elif group == "LOS":
+                group_df = basic_eda.group_by("length_of_stay")
+                st.bar_chart(data=group_df)
+            elif group == "County":
+                group_df = basic_eda.group_by("county")
+                st.bar_chart(data=group_df)
 
     if eda_select_box == "Box Plot":
         data_counter_container.header("Boxplot")
@@ -43,10 +47,3 @@ def eda_options_container():
             st.pyplot(fig)
 
 
-# def show_count_and_describe(column, col1, col2):
-#     with col1:
-#         group_df = basic_eda.group_by("age_group")
-#         st.bar_chart(data=group_df)
-#     with col2:
-#         group_df_description = basic_eda.data.loc[:, column]
-#         st.table(group_df_description)
