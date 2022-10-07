91d200897bcfb5c9086af9d0320ff684b0608746
Mengyao Xu <mengyaoxu1@gmail.com>
2022-10-06 22:39:56 -0400

    box plot and pie chart on streamlit

---
+++ web/components/coefficient_pie.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  6 20:20:05 2022
+
+@author: mengy
+"""
+
+from l2.models.LR1 import model as lr
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+
+@st.cache
+def plot_pie():
+    lr_model, X_train, y_train, X_test, y_pred, lr_features= lr()
+    total_coefficnet = lr_model.score(X_train, y_train)
+    coefficient_array = lr_model.coef_
+    intercept = lr_model.intercept_
+    labels = lr_features
+    values = [abs(i) for i in coefficient_array]
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    fig.update_layout(title_text='Coefficients of LR model')
+    fig.show()
+
+
+def main():
+    plot_pie()
+
+    
