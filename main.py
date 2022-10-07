b02be373b4f81c1d864bbb5d3d90636e2bf552e9
Mengyao Xu <mengyaoxu1@gmail.com>
2022-10-06 22:39:56 -0400

    box plot and pie chart on streamlit

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ Main entrance for streamlit
 import streamlit as st
 from streamlit_option_menu import option_menu
 from web.components.eda_options_container import *
+from web.components.coefficient_pie import plot_pie
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["EDA"],
+    selected = option_menu("Main Menu", ["EDA", "Model"],
         icons=['house'], menu_icon="cast", default_index=0)
 
 if selected == "EDA":
     eda_options_container()
+elif selected == 'Model':
+    plot_pie()
+    
 
