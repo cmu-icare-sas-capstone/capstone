d984046e9a86622f60762bf831ced3a628f7f785
Muyu Tong <61313159+mtong0@users.noreply.github.com>
2022-10-08 15:24:50 -0400

    dashboard

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from web.components.eda_options_container import eda_options_container
 from web.components.model_container import model_container
+from web.components.cube_container import olap_container
+import streamlit_nested_layout
+st.set_page_config(layout="wide")
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["EDA", "Model"],
+    selected = option_menu("Main Menu", ["EDA", "Model", "OLAP"],
         icons=['house'], menu_icon="cast", default_index=0)
 
 if selected == "EDA":
     eda_options_container()
 elif selected == 'Model':
     model_container()
-    
-
+elif selected == "OLAP":
+    col1, col2 = st.columns(2)
+    with col1:
+        olap_container(0)
+    with col2:
+        olap_container(1)
---
+++ web/constants/WebConstants.py
@@ -0,0 +1,10 @@
+readable_column_map = {
+    "Age Group": "age_group",
+    "Race": "race",
+    "LOS": "length_of_stay",
+    "Cost": "total_costs",
+    "CCS Diagnosis Description": "ccs_diagnosis_description",
+    "Area Name": "area_name",
+    "Facility Id": "facility_id",
+    "Gender": "gender"
+}
